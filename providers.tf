44dee7bd1deff0ffde7b260cd2eee193b9cfa85d
Omar <omar.tabios.daley@oracle.com>
2025-01-31 19:49:36 +0000

    Testing

--- providers.tf
+++ providers.tf
@@ -1,5 +1,9 @@
 provider "oci" {
-  region = "uk-london-1"
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
 }
 
 provider "oci" {
