30b7ab20b4eefebece4ed20cda02a94ba4d9490b
Omar <omar.tabios.daley@oracle.com>
2025-01-31 19:51:44 +0000

    Testing

--- vcn.tf
+++ vcn.tf
@@ -7,6 +7,14 @@ oci = {
     }
 } 
 
+provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
+}
+
 locals {
     regions = ["uk-london-1","eu-paris-1","eu-eumarseille-1"]
 }
